7b2bd66c1983db6e788eefa51c76ac5789b41faa
Josue Fontecha <jfontecha@gmail.com>
2014-03-07 18:21:51 +0100

    added probset3-opt2-ex1

---
+++ lesson3/probset3-opt2-ex1.py
@@ -0,0 +1,41 @@
+# By Dimitris_GR from forums
+# Modify Problem Set 31's (Optional) Symmetric Square to return True 
+# if the given square is antisymmetric and False otherwise. 
+# An nxn square is called antisymmetric if A[i][j]=-A[j][i] 
+# for each i=0,1,...,n-1 and for each j=0,1,...,n-1.
+
+def antisymmetric(A):
+	transpose= zip(*A)
+
+# test solution 1	
+	i = 0
+	for e in transpose:
+		j=0
+		for k in A:
+			if e[i]!=k[j]*(-1):
+				return False
+			j+=1
+		i+=1
+	return True
+
+# Test Cases:
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, 3], 
+                     [-2, -3, 0]])   
+#>>> True
+
+print antisymmetric([[0, 0, 0],
+                     [0, 0, 0],
+                     [0, 0, 0]])
+#>>> True
+
+print antisymmetric([[0, 1, 2], 
+                     [-1, 0, -2], 
+                     [2, 2,  3]])
+#>>> False
+
+print antisymmetric([[1, 2, 5],
+                     [0, 1, -9],
+                     [0, 0, 1]])
+#>>> False
