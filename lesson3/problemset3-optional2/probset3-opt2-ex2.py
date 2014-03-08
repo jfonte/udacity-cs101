655503774ad93a91a13a972228b91d7d3e3f519d
Josue Fontecha <jfontecha@gmail.com>
2014-03-08 15:46:13 +0100

    solved ex2 of probset3 opt2

---
+++ lesson3/problemset3-optional2/probset3-opt2-ex2.py
@@ -0,0 +1,86 @@
+# By Ashwath from forums
+# Given a list of lists representing a n * n matrix as input, 
+# define a  procedure that returns True if the input is an identity matrix 
+# and False otherwise.
+
+# An IDENTITY matrix is a square matrix in which all the elements 
+# on the principal/main diagonal are 1 and all the elements outside 
+# the principal diagonal are 0. 
+# (A square matrix is a matrix in which the number of rows 
+# is equal to the number of columns)
+
+def is_identity_matrix(matrix):
+
+  # check the matrix  is square
+  count=0
+  for e in matrix:
+    for i in e:
+      count+=1 # 4
+  if count != len(matrix)**2: #check length/width of matrix to be uniform
+    return False
+
+ # check 1s in diagonals and 0s outside it
+  j=0
+  for e in matrix:
+    k=0
+    for i in e:
+      if j==k: #check if the diagonals are 1s
+        if i!=1:
+          return False
+      else: #check if everything outside the diagonal are 0s
+        if i!=0:
+          return False
+      k+=1
+    j+=1
+  return True
+
+# Test Cases:
+
+matrix1 = [[1,0,0,0],
+           [0,1,0,0],
+           [0,0,1,0],
+           [0,0,0,1]]
+print is_identity_matrix(matrix1)
+#>>>True
+
+matrix2 = [[1,0,0],
+           [0,1,0],
+           [0,0,0]]
+
+print is_identity_matrix(matrix2)
+#>>>False
+
+matrix3 = [[2,0,0],
+           [0,2,0],
+           [0,0,2]]
+
+print is_identity_matrix(matrix3)
+#>>>False
+
+matrix4 = [[1,0,0,0],
+           [0,1,1,0],
+           [0,0,0,1]]
+
+print is_identity_matrix(matrix4)
+#>>>False
+
+matrix5 = [[1,0,0,0,0,0,0,0,0]]
+
+print is_identity_matrix(matrix5)
+#>>>False
+
+matrix6 = [[1,0,0,0],  
+           [0,1,0,2],  
+           [0,0,1,0],  
+           [0,0,0,1]]
+
+print is_identity_matrix(matrix6)
+#>>>False
+
+matrix7 = [[1, -1, 1],
+           [0, 1, 0],
+           [0, 0, 1]]
+print is_identity_matrix(matrix7)
+#>>>False           
+
+           
